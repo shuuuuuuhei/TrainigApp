e386f3da18f58d4821699c64fc90a0a17c724df2
shuhei <>
2021-04-06 21:51:04 +0900

    user.rbにスキーマの情報を追記

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  account                :string           not null
+#  age                    :integer          not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  gender                 :integer          not null
+#  height                 :float            not null
+#  name                   :string           not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  subscript              :boolean          not null
+#  weight                 :float            not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
