ca3382d89561fe31fe6556893fffc103464edd92
shuhei <>
2021-04-10 10:41:28 +0900

    ユーザーと紐づいたプロフィールモデルの作成

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -11,6 +11,15 @@
 #  weight     :float
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 class Profile < ApplicationRecord
     belongs_to :user
